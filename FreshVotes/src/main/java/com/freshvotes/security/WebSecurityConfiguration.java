0696c89a97abc09268539e60cddf59fdb73fd2fb
Shadma <shadmabi02@gmail.com>
2020-06-24 17:25:52 +0530

    adding password encoder

--- FreshVotes/src/main/java/com/freshvotes/security/WebSecurityConfiguration.java
+++ FreshVotes/src/main/java/com/freshvotes/security/WebSecurityConfiguration.java
@@ -1,16 +1,25 @@
 package com.freshvotes.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter{
 	
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("malihabakhshi01@gmail.com").password("123123").roles("USER");
+		auth.inMemoryAuthentication().passwordEncoder(getPasswordEncoder())
+		.withUser("malihabakhshi01@gmail.com").password(getPasswordEncoder().encode("123123")).roles("USER");
 	}
 	
 	@Override
