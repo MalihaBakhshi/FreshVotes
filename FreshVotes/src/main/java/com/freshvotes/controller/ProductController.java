d375b4a3b6467d664695957a109d3fd3bffae68e
Shadma <shadmabi02@gmail.com>
2020-06-25 20:14:16 +0530

    retrieving product data from db

--- FreshVotes/src/main/java/com/freshvotes/controller/ProductController.java
+++ FreshVotes/src/main/java/com/freshvotes/controller/ProductController.java
@@ -1,12 +1,30 @@
 package com.freshvotes.controller;
 
+import java.io.IOException;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.freshvotes.domain.Product;
+import com.freshvotes.domain.User;
+import com.freshvotes.repository.ProductRepository;
+
+import javassist.NotFoundException;
 
 @Controller
 public class ProductController {
+	
+	@Autowired
+	private ProductRepository productRepo;
 	
 	@GetMapping("/products")
 	public String getProducts(ModelMap model) {
@@ -14,9 +32,28 @@ public class ProductController {
 		return "product";
 	}
 	
+	@GetMapping("/products/{productId}")
+	public String product(@PathVariable Long productId, ModelMap model, HttpServletResponse response) throws NotFoundException, IOException {
+		Optional<Product> productOpt = productRepo.findById(productId);
+		if(productOpt.isPresent()) {
+			Product product = productOpt.get();
+			model.put("product", product);
+		}else {
+			response.sendError(HttpStatus.NOT_FOUND.value(),"Product with id "+productId+" was not found.");
+			return "product";
+		}
+		return "product";
+	}
+	
 	@PostMapping("/products")
-	public String createProducts() {
+	public String createProducts(@AuthenticationPrincipal User user) {
+		Product product=new Product();
 		
-		return "redirect:/products";
+		product.setPublished(false);
+		product.setUser(user);
+		
+		product=productRepo.save(product);
+		
+		return "redirect:/products/"+product.getId();
 	}
 }
